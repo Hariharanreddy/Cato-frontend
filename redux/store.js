e20b7579853b4d94a6ddcb5ae4d300aaa4a1ca1a
Hariharanreddy <animatrix536@gmail.com>
2024-05-29 16:13:49 +0530

    fix: changed backend url to EBS

--- redux/store.js
+++ redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./reducers/userReducer";
 import { otherReducer } from "./reducers/otherReducer";
 import { productReducer } from "./reducers/productReducer";
@@ -7,19 +7,18 @@ import { cartReducer } from "./reducers/cartReducer";
 //All the user data are stored in backend, rather than getting them stored in the variables
 //of the component, we will save them in this store, so that we can access them using useSelector
 export const store = configureStore({
-    //below is a combined reducer
-    reducer: {
-        user: userReducer,
-        other: otherReducer,
-        product: productReducer,
-        cart: cartReducer,
-    },
+  //below is a combined reducer
+  reducer: {
+    user: userReducer,
+    other: otherReducer,
+    product: productReducer,
+    cart: cartReducer,
+  },
 });
 
 // export const server = "https://marketo-ecommerce-server.onrender.com/api/v1"
 // export const server = "https://cato-server-production.up.railway.app/api/v1"
 //this one is from cyclic
-export const server = "https://gray-elephant-vest.cyclic.app/"
+export const server =
+  "http://cato-server-new-env.eba-ytzwhg59.ap-south-1.elasticbeanstalk.com/api/v1";
 // export const server = "http://localhost:5000/api/v1"
-
-
